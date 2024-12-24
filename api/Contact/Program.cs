3439e1ae594553c0da9cacaf5c8849be240bcd55
sobhan <sobhan@gmail.com>
2024-12-24 19:28:33 +0330

    initialize

---
+++ api/Contact/Program.cs
@@ -0,0 +1,50 @@
+﻿using Contact.model.table;
+using Dapper;
+using Microsoft.Data.SqlClient;
+namespace Contact
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionstring = "Data Source=.;Database=contacts;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            SqlConnection connection = new SqlConnection(connectionstring);
+            connection.Open();
+            //string sql = "insert into dbo.[user](username,password,fullname) values(@username,@password,@fullname)";
+            //usertable user = new usertable()
+            //{
+            //    username = "admin",
+            //    password = [],
+            //    fullname = "adminastrator"
+
+            //};
+            object param = new
+            {
+                username = "nima",
+                password = Array.Empty<byte>(),
+                fullname = "nimanazari"
+            };
+            object i = new
+            {
+                id = 1001
+            };
+
+            string sql = "select * from dbo.[user]";
+            IEnumerable<usertable> users=connection.Query<usertable>(sql);
+            foreach (usertable user in users)
+            {
+                Console.WriteLine($"username:{user.username},fullname:{user.fullname}");
+            }
+
+            //connection.Execute(sql,param);
+
+
+            connection.Close();
+
+
+        }
+      
+        
+    }
+}
